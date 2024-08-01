5dd7344ad4fcb2d3e1ec9c8080467d8ad4e10164
RyanM13 <115082882+RyanM13@users.noreply.github.com>
2024-08-01 11:00:04 +0000

    Fixed memory leak by passing by reference instead of making a copy of the Stack obj

--- main.cpp
+++ main.cpp
@@ -1,24 +1,19 @@
 #include "Stack.h"
 #include <iostream>
 
-void ArrStack(Stack);
+void ArrStack(Stack &);
 
 int main() {
   Stack numbers;
   ArrStack(numbers);
 }
 
-void ArrStack(Stack numbers) {
+void ArrStack(Stack &numbers) {
 
   numbers.push(5);
   numbers.push(6);
   numbers.print();
   numbers.pull();
   std::cout << std::endl;
-  numbers.print();
-
-  for (int i = 0; i < numbers.Sizeof(); i++) {
-    numbers.push(i);
-  }
   numbers.print();
 }
